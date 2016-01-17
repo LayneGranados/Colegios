02f35394d182f27f5d21a61ba1e7e2a920f1e7c2
Andres Orduz Grimaldo <andruz89@gmail.com>
2016-01-17 15:39:07 -0500

    espacio de ejemplo

--- GestionColegios/Codigo/GestionColegiosCliente/src/GUI/Util/ControllerComboAnio.java
+++ GestionColegios/Codigo/GestionColegiosCliente/src/GUI/Util/ControllerComboAnio.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 public class ControllerComboAnio {
     
     private AnioModal myAnio;
+    
     public CertificacionBusiness certificacionBusiness;
     public ConfiguracionBusiness configuracionBusiness;
     public MatriculaBusiness matriculaBusiness;
